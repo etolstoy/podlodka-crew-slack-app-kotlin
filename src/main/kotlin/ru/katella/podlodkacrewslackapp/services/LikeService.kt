6538fc607fe565c953a1c9fa4a071ce02f8746af
Eugene Katella <katellaevgenii@gmail.com>
2020-04-10 21:51:31 +0300

    basic parsing

---
+++ src/main/kotlin/ru/katella/podlodkacrewslackapp/services/LikeService.kt
@@ -0,0 +1,24 @@
+package ru.katella.podlodkacrewslackapp.services
+
+import com.slack.api.methods.MethodsClient
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+typealias UserId = String
+
+@Service
+class LikeService {
+
+    @Autowired
+    lateinit var client: MethodsClient
+
+    fun processLike(fromUser: UserId, toUser: UserId, likeString: String) {
+        println("user $fromUser send $likeString to $toUser")
+        client.chatPostMessage {
+            it.text("User ")
+                .username(fromUser)
+                .text(" sent $likeString to ")
+                .username(toUser)
+        }
+    }
+}
--- src/main/kotlin/ru/katella/podlodkacrewslackapp/services/MessageService.kt
+++ src/main/kotlin/ru/katella/podlodkacrewslackapp/services/MessageService.kt
@@ -3,34 +3,52 @@ package ru.katella.podlodkacrewslackapp.services
 import com.slack.api.bolt.App
 import com.slack.api.methods.MethodsClient
 import com.slack.api.methods.request.users.UsersInfoRequest
+import com.slack.api.model.block.RichTextBlock
+import com.slack.api.model.block.element.RichTextElement
+import com.slack.api.model.block.element.RichTextSectionElement
+import com.slack.api.model.block.element.UsersSelectElement
 import com.slack.api.model.event.MessageEvent
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import ru.katella.podlodkacrewslackapp.slack.SlackApp
+import kotlin.contracts.contract
 
 @Service
 class MessageService {
 
+
+
     @Autowired
-    lateinit var client: MethodsClient
+    lateinit var likeService: LikeService
 
     fun processMessage(message: MessageEvent) {
 
-        val sender = message.user
-        val text = message.text
-        val clientMsgId = message.clientMsgId
-        val channel = message.channel
-        val blocks = message.blocks
+        val currentUser = message.user
+        message.blocks.forEach { block ->
+            if (block is RichTextBlock) {
+                var shouldCheckTextField = true
+                var userId: String? = null
 
-        println(sender)
-        println(text)
-        println(clientMsgId)
-        println(channel)
-        blocks.forEach { println(it) }
+                val element = block.elements.first { it is RichTextSectionElement } as RichTextSectionElement
 
-        val userRequest = UsersInfoRequest.builder().user(sender).build()
-        val response = client.usersInfo(userRequest)
+                element.elements.forEach { innerPart ->
+                    when (innerPart) {
+                        is RichTextSectionElement.User -> {
+                            userId = innerPart.userId
+                            shouldCheckTextField = true
+                        }
+                        is RichTextSectionElement.Text -> {
+                            if (shouldCheckTextField && blockContainsIncrementLogic(innerPart.text)) {
+                                likeService.processLike(currentUser, userId!!, innerPart.text)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-        println("received user with name ${response.user.name}")
+    private fun blockContainsIncrementLogic(text: String): Boolean {
+        return text.contains("++")
     }
 }
