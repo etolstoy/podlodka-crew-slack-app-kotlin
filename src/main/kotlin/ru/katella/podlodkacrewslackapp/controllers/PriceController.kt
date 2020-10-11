14421f6a9fc91f9a2c01a6da6e30386a5254707c
Egor Tolstoy <igrekde@gmail.com>
2020-10-12 01:33:19 +0300

    Conformed updated offer_promo scheme

--- src/main/kotlin/ru/katella/podlodkacrewslackapp/controllers/PriceController.kt
+++ src/main/kotlin/ru/katella/podlodkacrewslackapp/controllers/PriceController.kt
@@ -8,15 +8,18 @@ import org.springframework.web.bind.annotation.RestController
 import ru.katella.podlodkacrewslackapp.services.PriceService
 
 @RestController
-@RequestMapping("/price")
+@RequestMapping("/offers_promo")
 class PriceController {
 
     @Autowired
     lateinit var priceService: PriceService
 
     @GetMapping
-    fun countPrice(@RequestParam offers: List<String>, promo: String?): Number {
-        return priceService.getPrice(offers, promo).bulkPrice
+    fun countPrice(@RequestParam(name = "offer_ids") offers: List<String>, promo: String?): Map<String, Number> {
+        val price = priceService.getPrice(offers, promo).bulkPrice
+        return mapOf(
+            "price" to price
+        )
     }
 
 }
